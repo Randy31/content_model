2cf0b1948fd4e53e2144650d15c8846069149b56
Rathna vel S <91729493+Randy31@users.noreply.github.com>
2024-01-09 16:27:42 +0530

    Add files via upload

---
+++ content creation/app.py
@@ -0,0 +1,27 @@
+import google.generativeai as genai
+import streamlit as st
+
+# Assuming you have already set your Google Cloud project and API key
+GOOGLE_API_KEY = 'your api key'  # Replace with your actual API key
+
+# Configure the library with your API key
+genai.configure(api_key=GOOGLE_API_KEY)
+
+def main():
+    st.title('Content Generator')
+
+    prompt = st.text_input('Enter your prompt:')
+    points = st.number_input('value:')
+    if st.button('Generate Content'):
+        generated_content = generate_content(f'Create product description for {prompt.lower()} in {points} points without heading')
+        st.subheader('Generated Content:')
+        st.write(generated_content)
+     
+def generate_content(prompt):
+    model = genai.GenerativeModel(model_name='gemini-pro')
+    response = model.generate_content(prompt)
+    result = response.text
+    return result
+
+if __name__ == '__main__':
+    main()
